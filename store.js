913929169a0e3ae40961bec7d4518931cb9f71ad
DEVLOGICaj <abdalraheemjniad@gmail.com>
2026-09-24 22:59:18 +0300

    Add files via upload

---
+++ store.js
@@ -0,0 +1,11 @@
+﻿const products = [
+    {
+        id: 1,
+        name: "wws",
+        description: "rsdxsax",
+        location: "scs",
+        image: "pro.jpg",
+        image2: "lap.jpg"
+        price: "1555"
+    },
+];
